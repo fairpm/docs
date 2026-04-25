adc89c6e9c66563df31f3b003f82e2e632cba7ca
Milana Cap <milanazzap@gmail.com>
2026-04-25 16:52:00 +0200

    Change baseURL to point to the home page. Update dependencies. Signed-off-by: Milana Cap <milanazzap@gmail.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://docs.fair.pm',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   organizationName: 'fairpm',
