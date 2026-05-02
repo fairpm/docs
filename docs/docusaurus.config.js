b3af88e7f949663665e9696ed3de4df751406645
Milana Cap <milanazzap@gmail.com>
2026-05-02 22:31:23 +0200

    Update tagline in docusaurus configuration
    Signed-off-by: Milana Cap <milanazzap@gmail.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'FAIR Documentation',
-  tagline: 'Docs are way too cool',
+  tagline: 'Docs are way too cool!!',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
